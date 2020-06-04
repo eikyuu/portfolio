4d1506ccef1035f6cd30d5731573a4c0b3da123a
eikyuu <v.duguet@laposte.net>
2020-06-04 23:32:39 +0200

    start

---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+const About = (props) => {
+  return (
+    <>
+      <div className="about">
+        <div className="about_block">
+          <h1>A PROPOS DE MOI...</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+import pdf from "./vincentduguet.pdf";
+const Home = (props) => {
+  return (
+    <>
+      <div className="home">
+        <div className="home_block">
+          <h1 className="home_block_h1">Développeur Web</h1>
+          <h2 className="home_block_h2">HTML &amp; CSS - REACT - SYMFONY</h2>
+          <a
+            href={pdf}
+            download="CV Vincent duguet"
+            className="mt-3 home_button"
+          >
+            TÉLÉCHARGER CV
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
