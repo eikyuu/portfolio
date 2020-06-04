598930941c86c4225406abae6f6ed5f2c6bf09fb
eikyuu <v.duguet@laposte.net>
2020-06-05 00:25:36 +0200

    add useState home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import pdf from "./vincentduguet.pdf";
 const Home = (props) => {
+  const [home] = useState({
+    h1: "Développeur Web",
+    h2: "HTML &amp; CSS - REACT - SYMFONY",
+    p: "en cours de construction",
+    a: "TÉLÉCHARGER CV",
+  });
   return (
     <>
       <div className="home">
         <div className="home_block">
-          <h1 className="home_block_h1">Développeur Web</h1>
-          <h2 className="home_block_h2">HTML &amp; CSS - REACT - SYMFONY</h2>
-          <p>En cours de construction</p>
+          <h1 className="home_block_h1">{home.h1}</h1>
+          <h2 className="home_block_h2">{home.h2}</h2>
+          <p>{home.p}</p>
           <a
             href={pdf}
             download="CV Vincent duguet"
             className="mt-3 home_button"
           >
-            TÉLÉCHARGER CV
+            {home.a}
           </a>
         </div>
       </div>
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = (props) => {
+  return (
+    <>
+      <h1>fff</h1>
+    </>
+  );
+};
+
+export default Portfolio;
