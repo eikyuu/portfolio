f3c0fd4a3c259ec06490f48c561ef61f7cc6c5d4
eikyuu <v.duguet@laposte.net>
2020-06-06 10:16:35 +0200

    key map

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -9,30 +9,35 @@ import crm from "./crm.png";
 const Portfolio = (props) => {
   const portfolio = [
     {
+      id: 1,
       img: abc,
       title: "Reseau ABC",
       text:
         "Application destinée à promouvoir les compétences des professionnels du réseau ABC à travers un système de recommandation des prestations et compétences des membres du réseau et de gestions des événements.(Symfony, Bootstrap)",
     },
     {
+      id: 2,
       img: prime,
       title: "Prime",
       text:
         "Application vitrine et de réservation pour le restaurant prime (Angular, NodeJS)",
     },
     {
+      id: 3,
       img: avidsen,
       title: "Avidsen",
       text:
         "Application vitrine (e-commerce) et de présentation de du groupe Avidsen (Symfony, Thelia)",
     },
     {
+      id: 4,
       img: travel,
       title: "Av-travel",
       text:
         "Application de réservation de voyage (Symfony, API Platform, React, Bootswatch)",
     },
     {
+      id: 5,
       img: crm,
       title: "API-CRM",
       text:
@@ -45,10 +50,10 @@ const Portfolio = (props) => {
         <h1 className="portfolio_h1 mt-5 mb-5">Réalisations</h1>
         <div className="portfolio_block">
           {portfolio.map((port) => (
-            <div className="portfolio_block_img">
+            <div key={port.id} className="portfolio_block_img">
               <img className="portfolio_img mb-3 ml-1" src={port.img} alt="" />
-              <div class="portofolio_text--hover">
-                <div class="text--hover">
+              <div className="portofolio_text--hover">
+                <div className="text--hover">
                   {port.title} <br /> {port.text}
                 </div>
               </div>
