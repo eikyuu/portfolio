11979bc796f58421da3ae8c018f7225b26246f11
eikyuu <v.duguet@laposte.net>
2020-06-20 18:39:22 +0200

    add quote

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -16,17 +16,26 @@ const About = (props) => {
         <div className="about_div_img">
           <img className="about_img" src={imgProfil} alt="" />
         </div>
-        <p className="about_p mt-5  mb-5">
+        <p className="about_p mt-5  mb-3">
           Autodidacte dans l'âme et passionné par le monde de l'image et du
           numérique, j'aime apprendre de nouvelles choses pour améliorer mes
           compétences. Ce que j'aime le plus, c'est l'art et la précision qui se
           cache derrière chaque design. Avec toutes mes capacités et mes
           connaissances, j'aurai le plus grand plaisir à me consacrer pleinement
           au développement de vos idées afin de les transformer en grands
-          projets. « La création d'un site web ou d'une application est un art.
-          On peint le code comme un tableau, une œuvre prend vie lentement sous
-          nos doigts. »
+          projets.
         </p>
+        <blockquote
+          className="quote mb-5"
+          cite="https://www.huxley.net/bnw/four.html"
+        >
+          <p>
+            « La création d'un site web ou d'une application est un art. On
+            peint le code comme un tableau, une œuvre prend vie lentement sous
+            nos doigts. »
+          </p>
+          <footer>-Vincent Duguet</footer>
+        </blockquote>
       </div>
     </section>
   );
