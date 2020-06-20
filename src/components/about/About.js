4183cdc26b41c2ebcfb5f7400b05df71c201d7f0
eikyuu <v.duguet@laposte.net>
2020-06-20 23:12:32 +0200

    modif button

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,8 +14,8 @@ const About = (props) => {
         <h1 className="about_h1 mt-5">A PROPOS DE MOI...</h1>
         <span className="about_span">QUI SUIS-JE ?</span>
         <div className="about_div_img">
-          <img className="about_img" src={imgProfil} alt="" />
-          <p className="about_p mt-5  mb-3">
+          <img className="about_img mt-4" src={imgProfil} alt="" />
+          <p className="about_p mt-5 mb-3">
             Autodidacte dans l'âme et passionné par le monde de l'image et du
             numérique, j'aime apprendre de nouvelles choses pour améliorer mes
             compétences. Ce que j'aime le plus, c'est l'art et la précision qui
