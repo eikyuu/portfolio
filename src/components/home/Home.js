329bb80a871b196ea674c7c71b17c8eb4f146042
eikyuu <v.duguet@laposte.net>
2020-06-20 02:16:04 +0200

    cursor and link css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -24,7 +24,12 @@ const Home = (props) => {
       <div data-aos="fade-right" className="home_block">
         <h1 className="home_block_h1">{home.h1}</h1>
         <h2 className="home_block_h2">{home.h2}</h2>
-        <a href={pdf} download="CV Vincent duguet" className="mt-3 home_button">
+        <a
+          style={{ textDecoration: "none", color: "white" }}
+          href={pdf}
+          download="CV Vincent duguet"
+          className="mt-3 home_button"
+        >
           {home.a}
         </a>
         <a href="https://www.linkedin.com/in/vincent-duguet/">
