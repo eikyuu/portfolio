1c22531037bfeb3120ab80b2615b22f5c6815238
eikyuu <v.duguet@laposte.net>
2020-07-03 15:30:53 +0200

    modif about p

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,18 +14,23 @@ const About = (props) => {
         <h1 className="about_h1 mt-5">A PROPOS DE MOI...</h1>
         <span className="about_span">QUI SUIS-JE ?</span>
         <div className="about_div_img">
-          <img className="about_img mt-4" src={imgProfil} alt="" />
-          <p className="about_p mt-5 mb-3">
+          <img className="about_img mt-5 mb-5" src={imgProfil} alt="" />
+          <p className="about_p mt-3 mb-3">
             Autodidacte dans l'âme et passionné par le monde de l'image et du
             numérique, j'aime apprendre de nouvelles choses pour améliorer mes
             compétences. Ce que j'aime le plus, c'est l'art et la précision qui
             se cache derrière chaque design. Avec toutes mes capacités et mes
             connaissances, j'aurai le plus grand plaisir à me consacrer
             pleinement au développement de vos idées afin de les transformer en
-            grands projets.
+            grands projets. <br />
+            <i style={{ color: "gray", marginTop: "50px" }}>
+              « La création d'un site web ou d'une application est un art. On
+              peint le code comme un tableau, une œuvre prend vie lentement sous
+              nos doigts. »
+            </i>
           </p>
         </div>
-        <blockquote
+        {/* <blockquote
           className="quote mb-5"
           cite="https://www.huxley.net/bnw/four.html"
         >
@@ -35,7 +40,7 @@ const About = (props) => {
             nos doigts. »
           </p>
           <footer>-Vincent Duguet</footer>
-        </blockquote>
+        </blockquote> */}
       </div>
     </section>
   );
