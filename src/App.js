a856b544858bc8b9c5d570d511231967740e1017
eikyuu <v.duguet@laposte.net>
2020-09-07 00:19:09 +0200

    scrollbarr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "./components/navbar/Nav";
 import Home from "./components/home/Home";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -10,8 +10,45 @@ import "./App.css";
 import About from "./components/about/About";
 
 function App() {
+  useEffect(() => {
+    const progressBar = document.querySelector(".scrollbar");
+
+    let totalHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    window.addEventListener("scroll", () => {
+      let progress = (document.documentElement.scrollTop / totalHeight) * 100;
+      progressBar.style.height = `${progress}%`;
+      progressBar.style.opacity = `${progress}%`;
+    });
+
+    const progressBarClick = document.querySelector(".clickScrollbar");
+
+    progressBarClick.addEventListener("click", (e) => {
+      let newPageScroll =
+        (e.clientY / progressBarClick.offsetHeight) * totalHeight;
+      window.scrollTo({
+        top: newPageScroll,
+        behavior: "smooth",
+      });
+    });
+
+    // pour gérer le resize, vu que la hauteur du site change.
+    let debounceResize;
+    window.addEventListener("resize", () => {
+      clearTimeout(debounceResize);
+      debounceResize = setTimeout(() => {
+        totalHeight = document.body.scrollHeight - window.innerHeight;
+      }, 250);
+    });
+  });
+
   return (
     <div className="App">
+      <div class="clickScrollbar"></div>
+      <div class="scrollbar"></div>
+
       <Nav />
       <Home />
       <About />
