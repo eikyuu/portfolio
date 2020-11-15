5447bf9be3ebba151b750ef32810e77b23aac52b
eikyuu <v.duguet@laposte.net>
2020-11-15 10:40:52 +0100

    remove import contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Nav from "./components/navbar/Nav";
 import Home from "./components/home/Home";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
-import Contact from "./components/contact/Contact";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
