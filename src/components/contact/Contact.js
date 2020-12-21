76f4b9d5a2c21f704cabc554242a3ae40f4c30c0
eikyuu <vduguet@laposte.net>
2020-12-21 08:29:32 +0100

    modification du footer

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,25 +6,12 @@ const Contact = () => {
     <div className="footer" id="contact">
       <div className="contain">
         <div className="coll">
-          <h1>Contact</h1>
           <ul>
-            <li>Vincent Duguet</li>
+            <li>© — Developed by Vincent Duguet</li>
             <li>Email : v.duguet.dev@gmail.com</li>
             <li>06.59.63.03.26</li>
           </ul>
         </div>
-        <div className="coll social">
-          <h1>Social</h1>
-          <ul>
-            <li>
-              <img src="https://svgshare.com/i/5fq.svg" alt="img" />
-            </li>
-            <li>
-              <img src="https://svgshare.com/i/5eA.svg" alt="img" />
-            </li>
-          </ul>
-        </div>
-        <div className="clearfix"></div>
       </div>
     </div>
   );
